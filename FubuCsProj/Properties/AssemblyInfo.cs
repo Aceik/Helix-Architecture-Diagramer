963795d955ad6454e6f1d1bcdba836e6d07c6d12
Thomas Tyack <thomastyack@aceik.com.au>
2018-04-03 21:53:47 +1000

    Initial FubuCSProj Commit

---
+++ FubuCsProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Runtime.InteropServices;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyDescription("Reworked")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.9.0.0")]
+[assembly: AssemblyProduct("Fubu.FubuCsProjFile")]
+[assembly: AssemblyTitle("Fubu.FubuCsProjFile")]
+[assembly: AssemblyTrademark("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: InternalsVisibleTo("FubuCsProjFile.Testing")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: ComVisible(true)]
+[assembly: Guid("8dfe06b8-3d22-4b22-93aa-fb5e5afe4b27")]
+
